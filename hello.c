c67632a352c6db22bf36946937bf899b0bba7dd5
MarcelaPaixao <marcela.paixao@edu.ufes.br>
2025-05-14 16:07:28 -0300

    nova func

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 //Programa criado por Marcela e Malu
 #include <stdio.h>
-int main(int argc, char *argv[]){
-    printf("hello world, %s\n", argv[0]);
+#include <string.h>
+int main(int argc, char *argv[]){ 
+    if(strcomp(argv[1], "pt-br")) printf("Ola, %s\n", argv[0]);
+    else printf("hello, %s\n", argv[0]);
     return 0;
 }
